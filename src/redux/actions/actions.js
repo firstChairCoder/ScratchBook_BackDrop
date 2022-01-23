bc597e87b5a36978f21177355876e99b25d8c6ae
firstChairCoder <md1qq7@gmail.com>
2022-01-23 07:48:49 +0100

    Redux refactoring

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,15 +1,14 @@
 /* eslint-disable no-unreachable */
-//Action types
+//Action types declaration
 export const GET_CATS = "GET_CATS";
-// export const GET_MORE_CATS = "GET_MORE_CATS";
 export const ADD_TO_FAVORITES = "ADD_TO_FAVORITES";
 export const REMOVE_FROM_FAVORITES = "REMOVE_FROM_FAVORITES";
 
-export const getCats = (amount, page) => {
+export const getCats = (amount) => {
   try {
     return async (dispatch) => {
       const response = await fetch(
-        `https://api.thecatapi.com/v1/breeds?limit=${amount}&page=${page}`,
+        `https://api.thecatapi.com/v1/breeds?limit=${amount}`,
         {
           headers: {
             "x-api-key": "68742e96-887f-4f9f-a489-ba3735d55609",
@@ -31,33 +30,6 @@ export const getCats = (amount, page) => {
     console.log(error);
   }
 };
-
-// export const getMoreCats = (amount, page) => {
-//   try {
-//     return async (dispatch) => {
-//       const response = await fetch(
-//         `https://api.thecatapi.com/v1/breeds?limit=${amount}&page=${page}`,
-//         {
-//           headers: {
-//             "x-api-key": "68742e96-887f-4f9f-a489-ba3735d55609",
-//           },
-//         }
-//       );
-//       const json = await response.json();
-//       if (json) {
-//         dispatch({
-//           type: GET_MORE_CATS,
-//           payload: json,
-//         });
-//       } else {
-//         console.log("Unable to fetch data from the API BASE URL!");
-//       }
-//     };
-//   } catch (error) {
-//     // Add custom logic to handle errors
-//     console.log(error);
-//   }
-// };
 
 export const addFavorites = (cat) => (dispatch) => {
   dispatch({
--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,6 +1,5 @@
 import {
   GET_CATS,
-  //   GET_MORE_CATS,
   ADD_TO_FAVORITES,
   REMOVE_FROM_FAVORITES,
 } from "../actions/actions";
@@ -14,8 +13,6 @@ function catsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_CATS:
       return { ...state, cats: action.payload };
-    // case GET_MORE_CATS:
-    //   return { ...state, cat: action.payload };
     case ADD_TO_FAVORITES:
       return { ...state, favorites: [...state.favorites, action.payload] };
     case REMOVE_FROM_FAVORITES:
