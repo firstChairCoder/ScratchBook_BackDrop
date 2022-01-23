32c2d869b567d1a8cb44f4979b15464817358003
firstChairCoder <md1qq7@gmail.com>
2022-01-23 07:48:08 +0100

    Usage of proper tabBar icons

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesome } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/Home";
 import LikedScreen from "../screens/Liked";
@@ -17,7 +16,7 @@ export const RootNavigator = () => {
         headerShown: false,
         tabBarActiveTintColor: "#212227",
         tabBarInactiveTintColor: "rgba(33,34,39,0.2)",
-        tabBarLabelStyle: { marginBottom: 17 },
+        tabBarLabelStyle: { marginBottom: 17, color: "#000" },
         tabBarStyle: {
           height: 73,
           borderTopWidth: 0,
