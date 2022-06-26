6a54fae650b72c399d6ad99d2213aed95b946392
firstChairCoder <md1qq7@gmail.com>
2022-06-26 07:55:06 +0100

    chore: add absolute path resolution

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,35 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [
+            ".js",
+            ".jsx",
+            ".ts",
+            ".tsx",
+            ".android.js",
+            ".android.tsx",
+            ".ios.js",
+            ".ios.tsx"
+          ],
+          root: ["./"],
+          alias: {
+            "@assets": "./assets",
+            "@components": "./src/components",
+            "@utils": "./src/utils",
+            "@constants": "./src/constants",
+            "@screens": "./src/screens",
+            "@navigation": "./src/navigation",
+            underscore: "lodash"
+          }
+        }
+      ],
+      "react-native-reanimated/plugin"
+    ]
   };
 };
